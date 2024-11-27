b08d3a18b3429ef020db955983da73642d183102
luiscarloshurtado <luiscarloshurtado@live.com>
2024-11-27 12:16:41 -0500

    'Update'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 import "../sdk/go.js/go.js";
 import "../sdk/go.js/go.router.js";
 import "./views.js";
+
+const lang = Go.lang().current() || "en";
+
+Go.import(`./lang/${lang}.js`, (lang) => {
+  Go.lang(lang);
+  Go.do("nav/start");
+});
