c50cab15e6f6b3943f57a70376e1601357a2412d
luiscarloshurtado <luiscarloshurtado@live.com>
2024-11-27 12:14:04 -0500

    'Update'

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 import "../sdk/go.js/go.js";
 import "../sdk/go.js/go.router.js";
+import "./views.js";
---
+++ js/views.js
@@ -0,0 +1,7 @@
+Go.views({
+  "/": {
+    name: "home",
+    html: "<div>HELLO WORLD</div>",
+    target: ".body",
+  },
+});
